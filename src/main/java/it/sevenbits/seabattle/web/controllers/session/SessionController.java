45e47747a394fbda26327c3ce350eb5375bb2665
artem <temathebst@gmail.com>
2024-04-03 12:19:10 +0600

    feat: added cross-origin

--- src/main/java/it/sevenbits/seabattle/web/controllers/session/SessionController.java
+++ src/main/java/it/sevenbits/seabattle/web/controllers/session/SessionController.java
@@ -12,15 +12,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +21,7 @@ import java.util.Optional;
  * session rest controller
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/session")
 @AllArgsConstructor
 public class SessionController {
--- src/main/java/it/sevenbits/seabattle/web/controllers/user/UserController.java
+++ src/main/java/it/sevenbits/seabattle/web/controllers/user/UserController.java
@@ -7,19 +7,13 @@ import it.sevenbits.seabattle.web.model.UserForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * user controller
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
