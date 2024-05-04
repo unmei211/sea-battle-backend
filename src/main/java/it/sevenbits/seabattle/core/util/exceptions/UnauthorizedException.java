7d5b5f54e5aafc5cbb936e0396a9c0f519651fbb
Fendel Nikita <fendelnikita@gmail.com>
2024-05-04 22:05:32 +0600

    feat: fix error 404

--- src/main/java/it/sevenbits/seabattle/core/util/exceptions/UnauthorizedException.java
+++ src/main/java/it/sevenbits/seabattle/core/util/exceptions/UnauthorizedException.java
@@ -1,8 +1,9 @@
 package it.sevenbits.seabattle.core.util.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UnauthorizedException extends RuntimeException{
     public UnauthorizedException(String message) {
         super(message);
